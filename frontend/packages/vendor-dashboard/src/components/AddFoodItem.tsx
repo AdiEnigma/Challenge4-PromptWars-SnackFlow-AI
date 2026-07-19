615b471abe56cd689954ed45624f37251150aa8e
AdiEnigma <adityabhowmik68@gmail.com>
2026-07-19 21:53:47 +0530

    yuh3

--- frontend/packages/vendor-dashboard/src/components/AddFoodItem.tsx
+++ frontend/packages/vendor-dashboard/src/components/AddFoodItem.tsx
@@ -11,7 +11,7 @@ import {
   InputAdornment,
   Grid,
 } from '@mui/material';
-import { SYNTHETIC_STALLS, SYNTHETIC_FOOD_ITEMS, FoodItemSynthetic } from '../../../../fan-interface/src/data/syntheticData';
+import { SYNTHETIC_STALLS, SYNTHETIC_FOOD_ITEMS, FoodItemSynthetic } from '../../../fan-interface/src/data/syntheticData';
 
 const PlusIcon = () => (
   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round">
